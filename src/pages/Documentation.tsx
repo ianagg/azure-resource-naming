48a0792dd0ea1c0ea1e9170ce8eb3202bc4a4da9
Iana <himishoichi@gmail.com>
2022-08-09 11:13:28 +0300

    Add styling to documentation

--- src/pages/Documentation.tsx
+++ src/pages/Documentation.tsx
@@ -1,10 +1,11 @@
 import DocumentsList from "../components/DocumentsList";
+import '../styles/Documentation.css'
 
 function Documentation() {
   return (
-    <>
+    <div className="documentation">
       <DocumentsList />
-    </>
+    </div>
   );
 }
 
