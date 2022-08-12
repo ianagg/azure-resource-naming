f3143c695da37ba355824758a4a82302125923aa
Iana <himishoichi@gmail.com>
2022-08-12 12:10:04 +0300

    Open in the new tab

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,14 @@ function Footer({ version }: any) {
   return (
     <footer className="footer">
       To report a bug or suggest an improvement, please, use this{' '}
-      <a href="https://github.com/ianagg/azure-resource-naming/issues">link</a>.
+      <a
+        href="https://github.com/ianagg/azure-resource-naming/issues"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        link
+      </a>
+      .
       <br />
       <Link to="/" className="siteName">
         Azure resource naming
